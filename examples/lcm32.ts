ff9e064581e5c101230c5ddec5f936ba08c2df1d
Carl Edwards <ecarl42@gmail.com>
2021-07-18 09:45:54 -0700

    use assembly for the LCM-32 computer
    Use the Ophis Assembler for an application for the LCM-32 by generating and utilizing an 8K ROM image.

--- examples/lcm32.ts
+++ examples/lcm32.ts
@@ -2,7 +2,6 @@ import { CpuRunner } from "../src/cpu6502/cpu-runner";
 import { Motherboard } from '../src/motherboard';
 import { Ram } from "../src/components/ram";
 import { Rom } from "../src/components/rom";
-import { InterruptGenerator } from "../src/components/interrupt-generator";
 import { Register, AddressMode } from "../src/cpu6502/internals";
 import { Opcode } from "../src/cpu6502/opcodes";
 import { Via6522 } from "../src/components/via-6522";
@@ -35,23 +34,19 @@ let runner = new CpuRunner({
 });
 var motherboard = new Motherboard(
     runner, 
-    200  // perform clock ticks every ~200ms
+    30  // perform clock ticks every ~30ms
     );
 
-// install 8k of RAM
+// install RAM
 motherboard.getComponentManager().add(
-    0x0000, 
-    new Ram(0x2000));
+    0x0000, // starting at $0000
+    new Ram(0x2000)); // 8k in size
 
-// install 8k of ROM
-let rom = new Rom(0x2000);
+// install ROM
+let rom = new Rom(0x2000); // 8k in size
 motherboard.getComponentManager().add(
-    0xE000, 
+    0xE000, // starting at $E000
     rom);
-
-// generate interrupts to blink LED-IRQ and LED-NMI
-motherboard.getComponentManager().add(0xA000, 
-    new InterruptGenerator());
 
 // VIA6522 #1
 let via6522_1 = new Via6522();
@@ -61,27 +56,21 @@ motherboard.getComponentManager().add(0x6000, via6522_1);
 let via6522_2 = new Via6522();
 motherboard.getComponentManager().add(0x8000, via6522_2);
 
-// Reset Code
-let reset_code = [
-    0xEA,               // nop
-    0x4C, 0x00, 0xE0    // jmp $E000 (back to the nop)
-];
-
-// copy in the reset/startup code
-for (var i = 0x0000; i < reset_code.length; i++) {
-    rom.program[i] = reset_code[i];
+// read in the assembled program generaged by the Ophis assembler
+var fs = require('fs'), binary = fs.readFileSync('lcm32.bin');
+for (var i = 0; i < binary.length; i++) {
+    rom.program[i] = binary[i];
 }
-// punch in the reset jump vectors
-rom.program[0x1FFC] = 0x00;
-rom.program[0x1FFD] = 0xE0;
 
 // install event listeners
 runner.on("readData", (context, addr, data) => {
-    console.log(`\t${context} - 0x${toHexByte(data)} read from $${toHexWord(addr)}`);
+    // uncomment to see the data being read
+    // console.log(`\t${context} - 0x${toHexByte(data)} read from $${toHexWord(addr)}`);
 });
 
 runner.on("writeData", (context, addr, data) => {
-    console.log(`\t${context} - 0x${toHexByte(data)} written to $${toHexWord(addr)}`);
+    // uncomment to see the data being written
+    // console.log(`\t${context} - 0x${toHexByte(data)} written to $${toHexWord(addr)}`);
 });
 
 runner.on("execOpcode", (reg: Register, opcode: Opcode) => {
@@ -106,7 +95,9 @@ runner.on("nmi", (currentPc: number) => {
 });
 
 via6522_1.on("PORTA", (prevValue:number, newValue:number) => {
-    console.log (`\n\nvia-1 - PORT-A LED: 0b${newValue.toString(2).padStart(8, '0')}\n\n`);
+    // LED is active low
+    const led = ~newValue & 0xFF;
+    console.log (`\n\nvia-1 - PORT-A LED: 0b${led.toString(2).padStart(8, '0')}\n\n`);
 });
 
 via6522_1.on("PORTB", (prevValue:number, newValue:number) => {
