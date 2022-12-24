d46fce8b6be5f06157ca4ff77f20fc8afe8202f1
carledwards <cedwards@bigskyhealth.com>
2022-12-24 09:07:15 -0800

    cleanup of un-used code

--- src/visual6502/web/all.js
+++ src/visual6502/web/all.js
@@ -12707,20 +12707,6 @@ export function setupTransistors() {
 	}
 }
 
-function hitBufferNode(ctx, i, w) {
-	var low = hexdigit(i & 0xf);
-	var mid = hexdigit((i >> 4) & 0xf);
-	var high = hexdigit((i >> 8) & 0xf);
-	ctx.fillStyle = '#' + high + 'F' + mid + 'F' + low + 'F';
-	for (i in w) {
-		drawSeg(ctx, w[i]);
-		ctx.fill();
-	}
-}
-
-function hexdigit(n) { return '0123456789ABCDEF'.charAt(n); }
-
-
 var recalclist = new Array();
 var recalcHash = new Array();
 var group = new Array();
@@ -12796,7 +12782,6 @@ function addNodeToGroup(i) {
 		});
 }
 
-
 function getNodeValue() {
 	if (arrayContains(group, ngnd)) return false;
 	if (arrayContains(group, npwr)) return true;
@@ -12809,7 +12794,6 @@ function getNodeValue() {
 	}
 	return false;
 }
-
 
 export function isNodeHigh(nn) {
 	return (nodes[nn].state);
